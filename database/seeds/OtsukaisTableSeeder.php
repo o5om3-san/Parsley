69f3b10bec61b6aeae2ca93c7f6c0b05ecdca427
takuto.someya <takuto.someya@rakuten.com>
2018-07-10 04:38:49 +0000

    seeder create

---
+++ database/seeds/OtsukaisTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class OtsukaisTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
