a21df5d8fe55a6bc350e7876020eac156c29e1d9
rumi.imai <rumi.a.imai@rakuten.com>
2018-07-10 05:15:41 +0000

    shopdatabase

--- database/seeds/ShopsTableSeeder.php
+++ database/seeds/ShopsTableSeeder.php
@@ -11,6 +11,23 @@ class ShopsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+     DB::table('shops')->insert([
+        [
+          'shop_name' => 'CH_9F_ConvenienceStore',
+          'address' => 'Crimson House 9F',
+        ],
+        [
+          'shop_name' => 'CH_22F_ConvenienceStore',
+          'address' => 'Crimson House 22F',
+        ],
+        [
+          'shop_name' => 'CH_9F_RakutenCafe',
+          'address' => 'Crimson House 9F',
+        ],
+        [
+          'shop_name' => 'CH_22F_RakutenCafe',
+          'address' => 'Crimson House 22F',
+        ],
+      ]);
     }
 }
