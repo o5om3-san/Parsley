e53b7933a48d13f9b9034a566551aee59f49bf3c
shoko.nagata <shoko.nagata@rakuten.com>
2018-07-12 07:48:52 +0000

    footer,navbar,color

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}">
     </head>
     <body>
         @include('commons.navbar')
@@ -20,5 +21,9 @@
 
             @yield('content')
         </div>
+       
     </body>
+    <footer>
+         @include('commons.footer')
+    </footer>
 </html>
