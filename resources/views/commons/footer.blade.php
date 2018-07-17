464c0075305d3c71ac2d4a31689f4adf1b85634f
takuto.someya <takuto.someya@rakuten.com>
2018-07-17 07:57:03 +0000

    2017/07/17

--- resources/views/commons/footer.blade.php
+++ resources/views/commons/footer.blade.php
@@ -3,15 +3,15 @@
         <div class="container">
             <div class="footer-header">
                 <a class="navbar-brand" href="/"></a>
+                
             </div>
             
                 <ul class="nav navbar-nav navbar-left">
                     @if (Auth::check())
-                    
-                        <li>{!! link_to_route('users.show','MyPage',['id' => Auth::id()]) !!}</li>
-                        <li>{!! link_to_route('otsukais.create','買い物に行く') !!}</li>
-                        <li>{!! link_to_route('otsukais.index','おつかいの一覧') !!}</li>
-                        <li>{!! link_to_route('logout.get','Logout') !!}</li>
+                        <li>{!! link_to_route('users.show','ホーム',['id' => Auth::id()],['class' => 'fas fa-home fa-lg fa-spin']) !!}</li>
+                        <li>{!! link_to_route('otsukais.create','買い物に行く',null,['class' => 'fas fa-shopping-cart fa-lg fa-spin']) !!}</li>
+                        <li>{!! link_to_route('otsukais.index','おつかいの一覧',null,['class' => 'fas fa-list-alt fa-lg']) !!}</li>
+                        <li>{!! link_to_route('logout.get','Logout',null,['class' => 'fas fa-sign-out-alt fa-lg']) !!}</li>
                     @else
                         <li>{!! link_to_route('login', 'ログイン') !!}</li>
                         <li>{!! link_to_route('register', '登録') !!}</li>
--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -8,7 +8,7 @@
                 　　<span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="/">Parsley</a>
+                <a class="navbar-brand" href="/">Parsley <i class="fab fa-product-hunt fa-lg fa-spin"></i></a>
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
