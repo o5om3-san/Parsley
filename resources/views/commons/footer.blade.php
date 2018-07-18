fb17f0fa9655aab5c942f3def0ff9a4baf5fcfd1
takuto.someya <takuto.someya@rakuten.com>
2018-07-18 00:54:10 +0000

    navbars

--- resources/views/commons/footer.blade.php
+++ resources/views/commons/footer.blade.php
@@ -1,15 +1,14 @@
 <header>
-    <nav class="navbar navbar-inverse navbar-static-top">
+    <nav class="navbar navbar-default">
         <div class="container">
             <div class="footer-header">
                 <a class="navbar-brand" href="/"></a>
-                
             </div>
             
                 <ul class="nav navbar-nav navbar-left">
                     @if (Auth::check())
-                        <li>{!! link_to_route('users.show','ホーム',['id' => Auth::id()],['class' => 'fas fa-home fa-lg fa-spin']) !!}</li>
-                        <li>{!! link_to_route('otsukais.create','買い物に行く',null,['class' => 'fas fa-shopping-cart fa-lg fa-spin']) !!}</li>
+                        <li>{!! link_to_route('users.show','Home',['id' => Auth::id()],['class' => 'fas fa-home fa-lg']) !!}</li>
+                        <li>{!! link_to_route('otsukais.create','買い物に行く',null,['class' => 'fas fa-shopping-cart fa-lg']) !!}</li>
                         <li>{!! link_to_route('otsukais.index','おつかいの一覧',null,['class' => 'fas fa-list-alt fa-lg']) !!}</li>
                         <li>{!! link_to_route('logout.get','Logout',null,['class' => 'fas fa-sign-out-alt fa-lg']) !!}</li>
                     @else
