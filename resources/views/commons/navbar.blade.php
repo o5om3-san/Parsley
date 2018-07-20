d099584c9da669cc102b8299706c953d039c75c7
rumi.imai <rumi.a.imai@rakuten.com>
2018-07-20 04:16:09 +0000

    updated

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -13,15 +13,13 @@
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
                     @if (Auth::check())
-                    
-                    <li class="name">{{ Auth::user()->name }}さん</li>
-                            <li>{!! link_to_route('users.show','MyPage',['id' => Auth::id()],['class' => 'fas fa-home fa-lg']) !!}</li>
+                            <li>{!! link_to_route('users.show',Auth::user()->name.'さんのPage',['id' => Auth::id()],['class' => 'fas fa-home fa-lg']) !!}</li>
                             <li>{!! link_to_route('otsukais.create','買い物にいく',null,['class' => 'fas fas fa-shopping-cart fa-lg']) !!}</li>
                             <li>{!! link_to_route('otsukais.index','一覧',null,['class' => 'fas fa-list-alt fa-lg']) !!}</li>
                             <li>{!! link_to_route('logout.get','Logout',null,['class' => 'fas fa-sign-out-alt fa-lg']) !!}</li>
                     @else
-                        <li><a href="/login"><img src="images/parsleylogin.png" width=280 alt="signup" class="img-responsive"></a></li>
-                        <li><a href="/register"><img src="images/parsleysignup.png" width=300 alt="signup" class="img-responsive"></a></li>
+                        <li><a href="/login"><img src="images/midorilogin.png" width=280 alt="signup" class="img-responsive"></a></li>
+                        <li><a href="/register"><img src="images/midorisignup.png" width=300 alt="signup" class="img-responsive"></a></li>
                     @endif
                 </ul>
             </div>
