27f05d8bb8538bfabdfc5373080f2ff89e09c2f5
takuto.someya <takuto.someya@rakuten.com>
2018-07-23 01:33:09 +0000

    edit.request

--- resources/views/requests/edit_request.blade.php
+++ resources/views/requests/edit_request.blade.php
@@ -17,14 +17,14 @@
                     </div>
                 </div>
             </div>
-            <div class='row order_card'>   
+            <div class='row order_card'>                    
                 <table>
                     <tr>
                         <td class='d_left'>商品：</td>
                         <td class='d_right'>
                                 <select name='item'>
                                     @foreach ($items as $item)
-                                        <option value={{$item->id}}> {{ $item->name }} </option>
+                                        <option value={{$item->id}} <?php if($onegai->item_id == $item->id){ echo 'selected'; }?> > {{ $item->name }} </option>
                                     @endforeach
                                 </select>
                             </span>
@@ -33,25 +33,22 @@
                     <tr>
                         <td class='d_left'>注文数：</td>
                         <td class='d_right'>
-                                {!! Form::selectRange('amount', 1, $otsukai->capacity-$amount, null) !!}
+                                {!! Form::selectRange('amount', 1, $otsukai->capacity-$amount+$onegai->amount, $onegai->amount) !!}
                         </td>
                     </tr>
-                     <tr>
+                    <tr>
                         <td class='d_left'>コメント：</td>
                         <td class='d_right'>
-                            {!! Form::textarea('comment', null, ['size' => '25x1']) !!}
+                            {!! Form::textarea('comment', $onegai->comment, ['size' => '25x1']) !!}
                         </td>
                     </tr>                   
                 </table>    
-                </div>     
-                <div class="row card_buttons">
+            </div>     
+            <div class="row card_buttons">
                     {!! Form::submit('更新', ['class' => 'btn btn-success']) !!}
-                </div>
-            
-            
-            
-            
-         </div> 
-
-
+                    {!! Form::close() !!}
+            </div>
+        </div>
+    </div>
+    
 @endsection
